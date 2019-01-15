b98ee5409b4d9620576d43ee3c5fbf67cd4e9bb8
David Nunez <david.nunez@yodle.com>
2019-01-15 00:14:10 -0500

    starting again from scratch

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-'use strict';
-const express = require('express');
-const nunjucks = require('nunjucks');
-
-const port = process.env.PORT || 3000;
-const app = express();
-
-
-app.set('view engine', 'nunjucks');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', function(req, res, next) {
-      res.render('views/acquisio-demo/index.html', {
-        metaDescription: '',
-        title: 'Acquisio Demo test'
-      });
-  });
-
-// Nunjucks Configurations
-const env = nunjucks.configure(__dirname, {
-  autoescape: false,
-  express: app
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'stupid ass world'}));
 });
-
-env.addFilter('htmlConvert', (str) => {
-  const lines = str.split('\n');
-  let res = '';
-
-  for (var i = 0; i < lines.length; i++) {
-    if (lines[i]) {
-      res += '<p>' + lines[i] + '</p>';
-    }
-  }
-  return res;
-});
-
-env.addFilter('jsonStringify', (str) => {
-  return JSON.stringify(str);
-});
-
 app.listen(port, function () {
- console.log('Example app listening on port !');
+ console.log(`Example app listening on port !`);
 });
