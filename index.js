a6e5e7b7a0b8069bc344941f909335c8063dac50
David Nunez <david.nunez@yodle.com>
2019-01-15 00:05:22 -0500

    removed unused middleware

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const nunjucks = require('nunjucks');
 
 const port = process.env.PORT || 3000;
 const app = express();
