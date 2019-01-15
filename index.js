5b399d5b7c9d6eccb5335c4df62f8e4066f57796
David Nunez <david.nunez@yodle.com>
2019-01-15 00:12:03 -0500

    added more middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 
 app.set('view engine', 'nunjucks');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', function(req, res, next) {
       res.render('views/acquisio-demo/index.html', {
@@ -13,6 +15,28 @@ app.use('/', function(req, res, next) {
         title: 'Acquisio Demo test'
       });
   });
+
+// Nunjucks Configurations
+const env = nunjucks.configure(__dirname, {
+  autoescape: false,
+  express: app
+});
+
+env.addFilter('htmlConvert', (str) => {
+  const lines = str.split('\n');
+  let res = '';
+
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i]) {
+      res += '<p>' + lines[i] + '</p>';
+    }
+  }
+  return res;
+});
+
+env.addFilter('jsonStringify', (str) => {
+  return JSON.stringify(str);
+});
 
 app.listen(port, function () {
  console.log('Example app listening on port !');
