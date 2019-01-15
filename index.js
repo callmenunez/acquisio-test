93a33d0753574ef030dea3aa60162c916af8aa79
David Nunez <david.nunez@yodle.com>
2019-01-15 11:47:33 -0500

    added static route for css files

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.set('view engine', 'nunjucks');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/public', express.static('public'));
 
 app.use('/', function (req, res) {
   return res.render('views/index.html');
-//  res.send(JSON.stringify({ Hello: 'stupid ass world'}));
 });
 
 // Nunjucks Configurations
 const env = nunjucks.configure(__dirname, {
   autoescape: false,
   express: app
-});
-env.addFilter('htmlConvert', (str) => {
-  const lines = str.split('\n');
-  let res = '';
-
-  for (var i = 0; i < lines.length; i++) {
-    if (lines[i]) {
-      res += '<p>' + lines[i] + '</p>';
-    }
-  }
-  return res;
 });
 
 app.listen(port, function () {
