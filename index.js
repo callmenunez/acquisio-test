3c23148c7e730491e24bb9c14d15f325a1e85b51
David Nunez <david.nunez@yodle.com>
2019-01-15 11:01:22 -0500

    added procfile

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'stupid ass world'}));
+const express = require('express');
+const nunjucks = require('nunjucks');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.set('view engine', 'nunjucks');
+
+app.use('/', function (req, res) {
+  return res.render('views/index.html');
+//  res.send(JSON.stringify({ Hello: 'stupid ass world'}));
 });
+
+// Nunjucks Configurations
+const env = nunjucks.configure(__dirname, {
+  autoescape: false,
+  express: app
+});
+env.addFilter('htmlConvert', (str) => {
+  const lines = str.split('\n');
+  let res = '';
+
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i]) {
+      res += '<p>' + lines[i] + '</p>';
+    }
+  }
+  return res;
+});
+
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
