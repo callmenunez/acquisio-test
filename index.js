efe896457ad18f18a61658fd94c4dcd4b67d38e9
David Nunez <david.nunez@yodle.com>
2019-01-17 16:57:02 -0500

    c

--- index.js
+++ index.js
@@ -28,23 +28,25 @@ app.use('/success', awsSave, function(req, res, next) {
 });
 
 app.use('/:customCode', awsRetrieve, function(req, res, next) {
-  function logResponseBody(req, res, next) {
-    var oldWrite = res.write,
-        oldEnd = res.end;
-    var chunks = [];
-    res.write = function (chunk) {
-      chunks.push(chunk);
-      oldWrite.apply(res, arguments);
-    };
-    res.end = function (chunk) {
-      if (chunk)
-        chunks.push(chunk);
-      var body = Buffer.concat(chunks).toString('utf8');
-      console.log(req.path, body);
-      oldEnd.apply(res, arguments);
-    };
-    next();
-  }
+  // function logResponseBody(req, res, next) {
+  //   var oldWrite = res.write,
+  //       oldEnd = res.end;
+  //   var chunks = [];
+  //   res.write = function (chunk) {
+  //     chunks.push(chunk);
+  //     oldWrite.apply(res, arguments);
+  //   };
+  //   res.end = function (chunk) {
+  //     if (chunk)
+  //       chunks.push(chunk);
+  //     var body = Buffer.concat(chunks).toString('utf8');
+  //     console.log(req.path, body);
+  //     oldEnd.apply(res, arguments);
+  //   };
+  //   next();
+  // }
+  
+  console.log(res.customCode);
   
   app.use(logResponseBody);
 
